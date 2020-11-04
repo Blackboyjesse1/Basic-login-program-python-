0fbde593030c470f2243c4d0e2fd8073518cecbf
Blackboyjesse1 <72275409+Blackboyjesse1@users.noreply.github.com>
2020-11-04 13:24:35 +0100

    Add files via upload

---
+++ Simple login program.py
@@ -0,0 +1,6 @@
+email_user = input('Enter your email: ')
+pass_user = input('Enter your password: ')
+# edit the part that says "example@email.com" and "examplepass" to your own password and email
+if email_user == 'example@email.com' and pass_user == 'examplepass':
+    print("Logged in!")
+    # write the rest of your code in this block
